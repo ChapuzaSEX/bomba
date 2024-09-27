a0dd0e16fe284c4e12c91f3b2b5c9c62616630a6
ChapuzaSEX <dylancortespasten@gmail.com>
2024-09-27 17:12:18 -0300

    prueba 1

--- soldado/character_body_2d.gd
+++ soldado/character_body_2d.gd
@@ -19,7 +19,7 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
+	#begieb
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
